caf96344aff5635febe1bb92856ca85035436f87
Celine <github@celinechole.com>
2020-06-28 18:48:29 -0400

    format

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-
 import MobilePageLinks from "./MobilePageLinks"
 import "./header.css"
 
-const Header = ({ siteTitle, tagline, author, contacts }) => {
+const Header = ({ siteTitle }) => {
   return (
     <header className="head-main">
       <div
