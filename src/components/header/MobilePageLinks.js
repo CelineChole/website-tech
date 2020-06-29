d8e7b3840ac975cfb7b76811e93968b091f3acb5
Celine <github@celinechole.com>
2020-06-29 19:15:05 -0400

    clean-up forever

--- src/components/header/MobilePageLinks.js
+++ src/components/header/MobilePageLinks.js
@@ -4,23 +4,9 @@ import { Link } from "gatsby"
 const MobilePages = () => {
   return (
     <div className="mobile-pages-main">
-      <div className="text-center">
-        <p className="d-inline p-4">
-          <Link to="/">
-            <span className="text-dark">Home</span>
-          </Link>
-        </p>
-        <p className="d-inline p-4">
-          <Link to="/about">
-            <span className="text-dark">About</span>
-          </Link>
-        </p>
-        <p className="d-inline p-4">
-          <Link to="/archive">
-            <span className="text-dark">Archive</span>
-          </Link>
-        </p>
-      </div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/archive">Archive</Link>
     </div>
   )
 }
