cd91219306d67c2984693def3399323724044887
Celine <github@celinechole.com>
2020-07-18 22:16:28 -0400

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import Sidebar from "../components/sidebar/Sidebar"
 import AboutMobileLinks from "../components/AboutMobileLinks"
+import siteLogo from "../images/siteLogo.png"
 
 const AboutPage = ({ data }) => {
   return (
@@ -19,10 +21,38 @@ const AboutPage = ({ data }) => {
             business analyst.
           </p>
           <p>
-            I work in a bootcamp as a web instructor. Web dev bit by bit is a
-            collection of resources that I found useful for me or for the
-            students I work with.
+            I work in a bootcamp as a web instructor. Web Dev Bit By Bit is a
+            collection of resources built for budding developers. You can
+            navigate through posts by clicking on tags, to load all posts click
+            on Archive.
           </p>
+          <p>
+            Some links that you might found useful:
+            <p>
+              <Link
+                to="/2020-07/vs-code-favorite-features-plugin/"
+                style={{ color: "#25EE4A", display: "block" }}
+              >
+                VS Code tips and tricks
+              </Link>
+
+              <Link to="/2019-08/tech-resources/" style={{ color: "#25EE4A" }}>
+                Tech resources
+              </Link>
+            </p>
+          </p>
+          <div style={{ textAlign: "center" }}>
+            <img
+              src={siteLogo}
+              style={{
+                display: "inline",
+                borderRadius: "10%",
+                width: "150px",
+                marginBottom: "3rem",
+                marginTop: "3rem",
+              }}
+            />
+          </div>
           <p>
             For the past 5 years I worked in the Intellectual Property industry
             as a Project Manager and later as a Business Analyst. I relocated to
@@ -36,7 +66,12 @@ const AboutPage = ({ data }) => {
             especially baking. I am also a yoga-addict, practicing nearly every
             day. If you would like some inspiration to practice, I created a
             library grouping YouTube videos I have been using:{" "}
-            <a target="_blank" href="https://yogilibrary.com/" rel="noreferrer">
+            <a
+              style={{ color: "#25EE4A" }}
+              target="_blank"
+              href="https://yogilibrary.com/"
+              rel="noreferrer"
+            >
               Yogi Library
             </a>
           </p>
